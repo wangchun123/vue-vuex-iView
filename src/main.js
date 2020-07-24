f621164e7fa41f05409ff00a5d82b005fb8c09af
qw <123@163.com>
2020-07-24 15:51:25 +0800

    config

--- src/main.js
+++ src/main.js
@@ -1,21 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-import router from "./router";
-import iview from "iview";
-import "iview/dist/styles/iview.css";
-import store from "./vuex/store";
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import iview from 'iview';
+import 'iview/dist/styles/iview.css';
+import store from './vuex/store';
 
-Vue.use(iview);
+Vue.use(iview, {
+  //全局配置
+  transfer: true,
+  size: 'large',
+  capture: false,
+  select: {
+    arrow: 'md-arrow-dropdown',
+    arrowSize: 20,
+  },
+});
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
   store,
   components: { App },
-  template: "<App/>"
+  template: '<App/>',
 });
